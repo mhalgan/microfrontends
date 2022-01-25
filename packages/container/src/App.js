5c93fbbd1bee88239ed2e46c603b90c62aa5c880
gcamargocamposrocha <gcamargocamposrocha@guidewire.com>
2022-01-25 11:49:46 +0000

    Add microfrontends lazy loading with progress bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+import Progress from "./components/Progress";
+
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -18,10 +20,12 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
